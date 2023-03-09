fd1dd5fa42f94c9c2d8aab0c69ed0ae9de7e04ae
SameetMoosavi <sameetmoosavi@yahoo.com>
2023-03-09 21:18:48 +0330

    add tailwind configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "purple": "#5E59C0",
+        "orange": "#F5A623",
+      }
+    },
+  },
+  plugins: [],
+}
