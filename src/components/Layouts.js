e72fee4014616aaa0ee61ef90d2b38cae0f3fcf0
SameetMoosavi <sameetmoosavi@yahoo.com>
2023-03-09 21:22:15 +0330

    update layouts

--- src/components/Layouts.js
+++ src/components/Layouts.js
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 const Layouts = ({children}) => {
   return (
     <>
-      <Header />
+      <header className="flex w-full h-fit bg-purple"><Header /></header>
       <main>{children}</main>
     </>
   )
