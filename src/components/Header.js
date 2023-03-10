4f59210d0750b92e196bba0c756cac93feee63d9
SameetMoosavi <sameetmoosavi@yahoo.com>
2023-03-10 23:03:30 +0330

    update header(change urls)

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,13 @@ import Link from "next/link"
 
 const Header = () => {
   return (
-    <div className="flex w-10/12 m-auto">
+    <div className="flex lg:w-10/12 w-full m-auto">
       <div className="flex ml-auto">
         <Link href='/'><h2 className="text-gray-300 text-xl py-2">{configs.logo}</h2></Link>
         <div className="flex mr-5">
           <Link href='/archives' className="text-gray-300 p-2 hover:bg-gray-300 hover:bg-opacity-10">فهرست</Link>
-          <Link href='/support' className="text-gray-300 p-2 hover:bg-gray-300 hover:bg-opacity-10">مشارکت کنندگان</Link>
-          <Link href='/contributors' className="text-gray-300 p-2 hover:bg-gray-300 hover:bg-opacity-10">حمایت مالی</Link>
+          <Link href='/contributors' className="text-gray-300 p-2 hover:bg-gray-300 hover:bg-opacity-10">مشارکت کنندگان</Link>
+          <a href='https://mahak-charity.org/online-payment' className="text-gray-300 p-2 hover:bg-gray-300 hover:bg-opacity-10" target="_blank">حمایت مالی</a>
           <a href='https://github.com/mthri/dfp-persian' className="text-gray-300 p-2 hover:bg-gray-300 hover:bg-opacity-10" target="_blank">دانلود</a>
         </div>
       </div>
